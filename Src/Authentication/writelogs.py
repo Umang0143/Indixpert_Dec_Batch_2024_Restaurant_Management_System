525c4bc4f093c07f4fff3f67b10da8fa78f4547d
Umang0143 <umangrajput1995@gmail.com>
2025-05-25 22:50:05 +0530

    Delete Src/Authentication/writelogs.py

--- Src/Authentication/writelogs.py
+++
@@ -1,5 +0,0 @@
-log=fr"D:\Indixpert 2025\Indixpert_Dec_Batch_2024_Restaurant_Management_System\Src\Logs\logs.txt"
-
-def writelogs(logs):
-    with open(log, "a") as file:
-        file.write(logs + "\n")
